ec84a02e0bb58cf94e1108774eb17cd1ec41b167
苏晓晴 <37541680+Suxiaoqinx@users.noreply.github.com>
2026-01-12 15:40:35 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'web_opefssl';
 
-export default nextConfig;
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
+
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repoName}` : '',
+  },
+}
+
+module.exports = nextConfig;
